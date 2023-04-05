f50cb651676de19e271b874e8939a80a8c666b39
Cesar Perez <cesarperezsampedro@gmail.com>
2023-04-05 20:52:23 +0200

    Funcionando firebase

--- src/app/peliculas/peliculas.component.ts
+++ src/app/peliculas/peliculas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';;
 import { Pelicula } from '../models/pelicula';
 import { PeliculasService } from '../peliculas.service';
 
@@ -9,16 +9,23 @@ import { PeliculasService } from '../peliculas.service';
 })
 export class PeliculasComponent implements OnInit {
   peliculas: Pelicula[];
+  //peliculas: Observable<Pelicula[]>;
 
   constructor(private peliculasService: PeliculasService) {
   }
 
   getPeliculas(): void {
-    this.peliculas = this.peliculasService.peliculas;
+    //this.peliculas = this.peliculasService.peliculas;
+    //this.peliculas = this.peliculasService.getAll();
   }
 
   ngOnInit(): void {
-    this.getPeliculas();
+    //this.getPeliculas();
+    this.peliculasService.getAll().subscribe(
+     peliculas => {
+      this.peliculas= peliculas;
+     }
+    )
   }
 
 }
