ad180d257c1e76f3a1aa0a81d66f859ccfb7f440
MarcosCervela <101353178+MarcosCervela@users.noreply.github.com>
2023-04-20 22:27:55 +0200

    Añadido codigo del typeScrypt de Actor-details.component me envia los valores vacios a la base de datos

--- src/app/actor-details/actor-details.component.ts
+++ src/app/actor-details/actor-details.component.ts
@@ -14,49 +14,70 @@ export class ActorDetailsComponent implements OnInit {
   actor: Actor;
   editForm: FormGroup;
   isEditMode: boolean = false;
+  nombreForm: string;
+  edadForm: number;
+  nacionalidadForm: string;
+  imagenForm: string;
+  clipForm: string;
+  vivoForm: boolean;
 
   constructor(
     private actoresService: ActoresService,
-    private route: ActivatedRoute,
-    private router: Router,
     private formBuilder: FormBuilder
+
+
+
   ) {
-    this.editForm = this.formBuilder.group({
-      nombre: [''],
-      edad: [''],
-      nacionalidad: [''],
-      imagen: [''],
-      clip: [''],
-      vivo: [false],
-    });
-  }
+    /*this.editForm = this.formBuilder.group({
+    nombre: [''],
+    edad: [''],
+    nacionalidad: [''],
+    imagen: [''],
+    clip: [''],
+    vivo: [false],
+  });*/
+}
 
   ngOnInit(): void {
     this.actoresService.findOneById(this.actorId).then((obj: any) => { this.actor = new Actor(obj.id, obj.nombre, obj.edad, obj.clip, obj.nacionalidad, obj.vivo, obj.imagen) });
-    this.editForm.setValue({
-      nombre: this.actor.nombre,
-      edad: this.actor.edad,
-      nacionalidad: this.actor.nacionalidad,
-      imagen: this.actor.imagen,
-      clip: this.actor.clip,
-      vivo: this.actor.vivo,
-    });
-  }
+this.editForm.setValue({
+
+  nombreForm : this.actor.nombre,
+  edadForm : this.actor.edad,
+  nacionalidadForm :  this.actor.nacionalidad,
+  imagenForm : this.actor.imagen,
+  clipForm : this.actor.clip,
+  vivoForm : this.actor.vivo
+})
+
+}
 
   toggleEditMode() {
     this.isEditMode = !this.isEditMode;
   }
 
-  saveChanges() {
 
-    const updatedActor: Actor = {
-      ...this.actor,
-      ...this.editForm.value,
-    };
 
-    console.log('updatedActor', updatedActor) // updatedActor no compone bien los datos del formulario, por eso no se guarda 
-    this.actoresService.update(updatedActor);
-    this.actor = updatedActor;
-    this.isEditMode = false;
+  saveActorDetails() {
+
+    if (this.actor.nombre != this.nombreForm) {
+      this.actor.nombre = this.nombreForm;
+    }
+    if (this.actor.edad != this.edadForm) {
+      this.actor.edad = this.edadForm;
+    }
+    if (this.actor.nacionalidad != this.nacionalidadForm) {
+      this.actor.nacionalidad = this.nacionalidadForm;
+    }
+    if (this.actor.imagen != this.imagenForm) {
+      this.actor.imagen = this.imagenForm;
+    }
+    if (this.actor.clip != this.clipForm) {
+      this.actor.clip = this.clipForm;
+    }
+    if (this.actor.vivo != this.vivoForm) {
+      this.actor.vivo = this.vivoForm;
+    }
+    this.actoresService.update(this.actor);
   }
 }
