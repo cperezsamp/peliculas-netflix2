7ee1d54537541b59cbd1325626a29f4b45e12538
Cesar Perez <cesarperezsampedro@gmail.com>
2023-04-22 12:18:57 +0200

    Modificado pipe

--- src/app/buscador.pipe.ts
+++ src/app/buscador.pipe.ts
@@ -8,29 +8,31 @@ import { Actor } from './models/actor';
 })
 export class BuscadorPipe implements PipeTransform {
 
-  actores: Actor[];
+  //actores: Actor[];
 
   constructor(private actoresService: ActoresService) {
     //this.getActores();
   }
 
 
-  async getActores(){
+  /*async getActores(){
     this.actoresService.getAll().subscribe(
       response => {
         this.actores= response;
       }
     )
     console.log(this.actores);
-  }
+  }*/
   transform(value: any, ...args: any[]): any {
-    this.getActores();
-    const result = [];
+    //this.getActores();
+    const result: any = [];
+    (async () => {
     for (let actor of value) {
       if ((actor.nombre.toUpperCase()).includes(args[0].toUpperCase()) && (actor.edad > args[1] && actor.edad < args[2]) && (actor.nacionalidad.toUpperCase()).includes(args[3].toUpperCase())) {
         result.push(actor);
       }
     }
+    })();
     return result;
   }
 
